2bb13d45bb2c448b9097f2dcbd618ccd64e6916f
psegenmark <psegenmark@gmail.com>
2017-12-09 22:29:17 -0800

    ds

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 require('dotenv').config();
 
-var connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+var connection = mysql.createConnection("mysql://bb9e84d6de20b8:ad14a6a5@us-cdbr-iron-east-05.cleardb.net/heroku_7d10f2e6fa76b42?reconnect=true");
 
 connection.connect(function (err) {
     if (err) {
