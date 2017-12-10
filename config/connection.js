b900907f5edcd1d893acf5d30ab303a4f3c1d09d
psegenmark <psegenmark@gmail.com>
2017-12-09 22:38:22 -0800

    dsss

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,28 @@
 var mysql = require('mysql');
+
 require('dotenv').config();
 
-var connection = mysql.createConnection("mysql://bb9e84d6de20b8:ad14a6a5@us-cdbr-iron-east-05.cleardb.net/heroku_7d10f2e6fa76b42?reconnect=true");
+if (process.env.CLEARDB_DATABASE_URL) {
+    var connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+}
+else
+{
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: process.env.password,
+        database: 'burgers_db'
+    });
+};
 
 connection.connect(function (err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
         return;
     }
+
     console.log('connected as id ' + connection.threadId);
 });
-module.exports = connection;
 
+module.exports = connection;
   
